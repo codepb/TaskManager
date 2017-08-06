14a14f39b265ada52a8f86fb2e358c5d41ca406d
codepb <paul.j.burgess@outlook.com>
2017-08-06 17:40:07 +0100

    Improving time display

--- app/components/TaskDisplay.js
+++ app/components/TaskDisplay.js
@@ -13,18 +13,13 @@ export default class TaskDisplay extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      timeSpent: props.task.TimeSpent
+      timeSpent: this.displayTime(props.task.currentTimeSpent())
     };
+    this.setupInterval(props);
   }
 
-  componentDidUpdate(prevProps) {
-    if (this.props.task.Running && !prevProps.task.Running) {
-      this.interval = setInterval(() => {
-        this.tick(this.props.task.currentTimeSpent());
-      }, 1000);
-    } else if (!this.props.task.Running && typeof (this.interval) !== 'undefined') {
-      clearInterval(this.interval);
-    }
+  componentWillReceiveProps(nextProps) {
+    this.setupInterval(nextProps, this.props);
   }
   componentWillUnmount() {
     if (typeof (this.interval) !== 'undefined') {
@@ -32,11 +27,40 @@ export default class TaskDisplay extends Component {
     }
   }
 
+  setupInterval(nextProps, prevProps) {
+    if (nextProps.task.Running && (!prevProps || !prevProps.task.Running)) {
+      this.interval = setInterval(() => {
+        this.tick(nextProps.task.currentTimeSpent());
+      }, 1000);
+    } else if (!nextProps.task.Running && typeof (this.interval) !== 'undefined') {
+      clearInterval(this.interval);
+    }
+  }
+
   tick(timeSpent) {
-    this.setState({ timeSpent });
+    this.setState({ timeSpent: this.displayTime(timeSpent) });
+  }
+
+  displayTime(timeSpent) {
+    const hours = Math.floor(timeSpent / 3600);
+    const totalSeconds = timeSpent % 3600;
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = parseInt(totalSeconds % 60);
+    return `${hours}:${this.pad(minutes, 2)}:${this.pad(seconds, 2)}`;
+  }
+
+  pad(n, width, z) {
+    const zValue = z || '0';
+    const nValue = `${n}`;
+    return nValue.length >= width ? nValue : new Array((width - nValue.length) + 1).join(zValue) + nValue;
   }
 
   render() {
-    return <li key={this.props.task.Id} className={this.props.task.Running ? styles.running : ''}><div className="shortcut">{this.props.shortcut}:&nbsp;</div><div className="task">{this.props.task.Task}</div><div className="time">{this.state.timeSpent.toFixed(0)}</div></li>;
+    return (
+      <li key={this.props.task.Id} className={this.props.task.Running ? styles.running : ''}>
+        <div className={styles.shortcut}>{this.props.shortcut}:&nbsp;</div>
+        <div className={styles.task}>{this.props.task.Task}</div>
+        <div className={styles.time}>{(this.state.timeSpent)}</div>
+      </li>);
   }
 }
